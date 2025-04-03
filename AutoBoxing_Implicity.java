77ad5d9836276d4e50a059bbf3d9de5469d3d849
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-04-03 11:41:36 +0530

    Add files via upload

---
+++ AutoBoxing_Implicity.java
@@ -0,0 +1,14 @@
+package com.Wrapper_Classes;
+
+public class AutoBoxing_Implicity 
+{
+
+	public static void main(String[] args) 
+	{
+		int a=20;
+		Integer a1=a;
+		System.out.println(a+a1);
+
+	}
+
+}
