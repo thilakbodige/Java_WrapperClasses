8953b479b4035b97d02091285810d1f9b22b5bc3
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-04-04 15:26:45 +0530

    Add files via upload

---
+++ AutoUnBoxing_Implicity.java
@@ -0,0 +1,16 @@
+package com.Wrapper_Classes;
+
+public class AutoUnBoxing_Implicity 
+{
+	public static void main(String[] args) 
+	{
+		Integer a=10;
+		int a1=a;
+		Double d=2.0;
+		double d1=d;
+		System.out.println(a+" -> Premitive: "+a1);
+		System.out.println(d+" -> Premitive: "+d1);
+
+	}
+
+}
