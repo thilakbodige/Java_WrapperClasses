aa07ad9d9828f31846ba6bb6aded167aa94caca1
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-04-04 15:25:55 +0530

    Add files via upload

---
+++ AutoUnBoxing_Explicity.java
@@ -0,0 +1,16 @@
+package com.Wrapper_Classes;
+
+public class AutoUnBoxing_Explicity //typevalueOf like *** doublevalue***
+{
+	public static void main(String[] args) 
+	{
+		Integer a=10;
+		int a1=a.intValue();
+		Double d=2.0;
+		double d1=d.doubleValue();
+		System.out.println(a+" ->Premitive :"+a);
+		System.out.println(d+" -> premitive : "+d1);
+
+	}
+
+}
