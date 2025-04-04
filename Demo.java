7319bd7c1600cbbdfe48fc3404fb2b31fdf7cb9e
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-04-04 15:27:02 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,13 @@
+package com.Wrapper_Classes;
+
+public class Demo 
+{
+	public static void main(String[] args) 
+	{
+		Integer a=20;
+		Double d=4.019;
+		}
+	}
+//Byte,Short,Long,Integer,Double,Float,Character,Boolean are ****Inbuilt Methods****//
+
+//Wrapper Classes are Present Inside ****java.lang.package****//
